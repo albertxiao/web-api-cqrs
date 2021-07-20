3c6e1f38fd86374753035990f0830f1271a576c3
mahendraputra21 <31196162+mahendraputra21@users.noreply.github.com>
2021-07-20 20:35:08 +0800

    Create DBContextTransactionPipelineBehavior.cs

---
+++ Context/DBContextTransactionPipelineBehavior.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace WebApiCQRS.Context
+{
+    /// <summary>
+    /// Adds transaction to the processing pipeline
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public class DBContextTransactionPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ApplicationContext _context;
+
+        public DBContextTransactionPipelineBehavior(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            TResponse result = default(TResponse);
+
+            try
+            {
+                _context.BeginTransaction();
+
+                result = await next();
+
+                _context.CommitTransaction();
+            }
+            catch (Exception)
+            {
+                _context.RollbackTransaction();
+                throw;
+            }
+
+            return result;
+        }
+    }
+}
